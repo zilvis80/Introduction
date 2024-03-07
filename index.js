76a8d8578b3e266069afa0787ec317b759c2b9b8
zilvinas <zilvis80@mail.ru>
2024-03-07 21:41:25 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// const express = require('express');
+// const app = express();
+// app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World');
+// });
+// app.get('/about', (req, res) => {
+//   res.json('About page');
+// });
+// app.listen(300, () => {
+//   console.log('server is running on port 3000');
+// });
+
+// app.get('/book', (req, res) => {
+//   res.send('All book');
+// });
+
+// app.get('/book/:id', (req, res) => {
+//   console.log(req.params);
+//   res.send(`book with id ${req.params.id}`);
+// });
